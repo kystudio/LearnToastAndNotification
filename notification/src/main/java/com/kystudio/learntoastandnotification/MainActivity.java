95463df2eb0805f49256048aa9ea6f3771e246a0
kystudio <yuan012308@hotmail.com>
2019-06-10 10:31:08 +0800

    update notification (android O notification channel)

--- notification/src/main/java/com/kystudio/learntoastandnotification/MainActivity.java
+++ notification/src/main/java/com/kystudio/learntoastandnotification/MainActivity.java
@@ -1,7 +1,9 @@
 package com.kystudio.learntoastandnotification;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.NotificationCompat;
@@ -11,24 +13,29 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     public static final int NOTIFICATION_ID = 1011;
     public int counter;
-    private Button btnCreateNotification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCreateNotification = findViewById(R.id.btnCreateNotification);
+        Button btnCreateNotification = findViewById(R.id.btnCreateNotification);
         btnCreateNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 counter++;
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this);
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel("channel01", "channel_name",
+                            NotificationManager.IMPORTANCE_DEFAULT);
+                    manager.createNotificationChannel(channel);
+                }
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "channel01");
                 builder.setSmallIcon(R.mipmap.ic_launcher);
                 builder.setContentTitle("哇，有" + counter + "个新消息！");
                 builder.setContentText("你已经可以创建新的Notification了。");
                 Notification notification = builder.build();
-                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
                 manager.notify(NOTIFICATION_ID, notification);
             }
         });
